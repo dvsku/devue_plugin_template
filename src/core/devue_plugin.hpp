eb981c1528bb230de0bee2109809e65ca4238d80
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-29 13:10:59 +0100

    Convert plugin to use internal impl

--- src/core/devue_plugin.hpp
+++ src/core/devue_plugin.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "devue_plugin_base.hpp"
+#include "devue_plugin_impl.hpp"
 
 namespace devue::plugins {
-    class dv_model_plugin : public devue_plugin_base {
+    class devue_plugin : public devue_plugin_base {
     public:
         virtual void impl_init() final override;
 
@@ -12,6 +13,9 @@ namespace devue::plugins {
         std::vector<file_type> impl_get_texture_types()                                   final override;
         devue_plugin_model     impl_import_model(const std::filesystem::path& filepath)   final override;
         devue_plugin_texture   impl_import_texture(const std::filesystem::path& filepath) final override;
+
+    private:
+        devue_plugin_impl impl;
     };
 }
 
